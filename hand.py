22fc7b8b929910a9d7fb8cdd3c108d0917529d5f
SoniyaChawla1494 <42843095+SoniyaChawla1494@users.noreply.github.com>
2019-05-11 20:58:54 -0400

    Variable name changed

--- hand.py
+++ hand.py
@@ -5,65 +5,66 @@ import math
 import os
 import time
 import pyautogui
-os.chdir("E:/Hand-Gesture-Recognition-for-Presentation-Process-in-Python-master")
-def h_crop(crop_img,img):
+import image_cleanup
+
+def hand_crop(crop_img,img):
     grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
     
     # applying gaussian blur
-    value = (35, 35)
-    blurred = cv2.GaussianBlur(grey, value, 0)
+    # applying gaussian blur
+    gauss_blur = image_cleanup.gaussian_method(grey)
 
-    # thresholdin: Otsu's Binarization method
-    _, thresh1 = cv2.threshold(blurred, 127, 255,
-                               cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    # thresholding: Otsu's Binarization method
+    thresh_img = image_cleanup.otsu_method(gauss_blur)
 
     # show thresholded image
-    cv2.imshow('Thresholded', thresh1)
+    cv2.imshow('Thresholded', thresh_img)
 
     # check OpenCV version to avoid unpacking error
     (version, _, _) = cv2.__version__.split('.')
 
     if version == '3':
-        image, contours, hierarchy = cv2.findContours(thresh1.copy(), \
+        image, contours, hierarchy = cv2.findContours(thresh_img.copy(), \
                cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     else:
-        contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, \
+        contours, hierarchy = cv2.findContours(thresh_img.copy(),cv2.RETR_TREE, \
                cv2.CHAIN_APPROX_NONE)
 
     # find contour with max area
-    cnt = max(contours, key = lambda x: cv2.contourArea(x))
+    max_contour = max(contours, key = lambda x: cv2.contourArea(x))
 
     # create bounding rectangle around the contour (can skip below two lines)
-    x, y, w, h = cv2.boundingRect(cnt)
+    x, y, w, h = cv2.boundingRect(max_contour)
     cv2.rectangle(crop_img, (x, y), (x+w, y+h), (0, 0, 255), 0)
 
     # finding convex hull
-    hull = cv2.convexHull(cnt)
+    hull = cv2.convexHull(max_contour)
 
-    # drawing contours
-    drawing = np.zeros(crop_img.shape,np.uint8)
-    cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
-    cv2.drawContours(drawing, [hull], 0,(0, 0, 255), 0)
+    # draw_contours contours
+    draw_contours = np.zeros(crop_img.shape,np.uint8)
+    cv2.drawContours(draw_contours, [max_contour], 0, (0, 255, 0), 0)
+    cv2.drawContours(draw_contours, [hull], 0,(0, 0, 255), 0)
 
     # finding convex hull
-    hull = cv2.convexHull(cnt, returnPoints=False)
+    hull = cv2.convexHull(max_contour, returnPoints=False)
 
-    # finding convexity defects
-    defects = cv2.convexityDefects(cnt, hull)
-    count_defects = 0
-    cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
+    # finding convexity convex_defects
+    convex_defects = cv2.convexityconvex_defects(max_contour, hull)
+    count_convex_defects = 0
+    cv2.drawContours(thresh_img, contours, -1, (0, 255, 0), 3)
 
-    # applying Cosine Rule to find angle for all defects (between fingers)
-    # with angle > 90 degrees and ignore defects
-    for i in range(defects.shape[0]):
-        s,e,f,d = defects[i,0]
+    # applying Cosine Rule to find angle for all convex_defects (between fingers)
+    # with angle > 90 degrees and ignore convex_defects
+    for i in range(convex_defects.shape[0]):
+        s,e,f,d = convex_defects[i,0]
 
-        start = tuple(cnt[s][0])
-        end = tuple(cnt[e][0])
-        far = tuple(cnt[f][0])
-
+        start = tuple(max_contour[s][0])
+        end = tuple(max_contour[e][0])
+        far = tuple(max_contour[f][0])
+        if d>9000:
+            count_convex_defects+=1
         # find length of all sides of triangle
-        a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+        '''a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
         b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
         c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
 
@@ -72,28 +73,28 @@ def h_crop(crop_img,img):
 
         # ignore angles > 90 and highlight rest with red dots
         if angle <= 90:
-            count_defects += 1
+            count_convex_defects += 1
             cv2.circle(crop_img, far, 1, [0,0,255], -1)
-        #dist = cv2.pointPolygonTest(cnt,far,True)
+        #dist = cv2.pointPolygonTest(max_contour,far,True)
 
         # draw a line from start to end i.e. the convex points (finger tips)
         # (can skip this part)
         cv2.line(crop_img,start, end, [0,255,0], 2)
-        cv2.circle(crop_img,far,5,[0,0,255],-1)
+        cv2.circle(crop_img,far,5,[0,0,255],-1)'''
 
     # define actions required
+    count_convex_defects+=1
+    if count_convex_defects==2 or count_convex_defects==1:
+        keyboard.press_and_release('-')
+
         
-    if count_defects == 4:
-        #pyautogui.press('right')  
-        #os.startfile('cal')
-        keyboard.press_and_release('-')   
     else:
         cv2.putText(img,"0", (50, 50),\
             cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-    
+    ha=0
     
     #time.sleep(1)
     # show appropriate images in windows
     #cv2.imshow('Gesture', img)
-    all_img = np.hstack((drawing, crop_img))
+    all_img = np.hstack((draw_contours, crop_img))
     cv2.imshow('Contours', all_img)
--- main_file.py
+++ main_file.py
@@ -5,25 +5,21 @@ import math
 import os
 import time
 import pyautogui
-from point import p_crop
-from hand import h_crop
-from fin import f_crop
-from fist import fs_crop
-from thumbdown import t_crop
-from okay import ok_crop
+from point import point_crop
+from hand import hand_crop
+from fin import fin_crop
+from thumbdown import thumbdown_crop
 
-os.chdir("E:/Hand-Gesture-Recognition-for-Presentation-Process-in-Python-master")
-h1_cascade=cv2.CascadeClassifier('hand.xml')
-okay_cascade = cv2.CascadeClassifier('ok.xml')
+
+hand_cascade=cv2.CascadeClassifier('hand.xml')
 point_cascade = cv2.CascadeClassifier('point1.xml')
 fin_cascade=cv2.CascadeClassifier('fin_2.xml')
-fist_cascade=cv2.CascadeClassifier('fist.xml')
-thumbdown_cascade = cv2.CascadeClassifier('thumbdown.xml')
-cap = cv2.VideoCapture(0)
-#ca=0
 
-while(cap.isOpened()):    
-        _,img=cap.read()
+thumbdown_cascade = cv2.CascadeClassifier('thumbdown.xml')
+capture = cv2.Videocaptureture(0)
+
+while(capture.isOpened()):    
+        _,img=capture.read()
         
         cv2.rectangle(img, (300,300), (100,100), (0,255,0),0)
         crop_img = img[100:300, 100:300]
@@ -31,57 +27,51 @@ while(cap.isOpened()):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         point=point_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3,flags=0, minSize=(100,80))
         fin=fin_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3,flags=0, minSize=(100,80))
-        hand=h1_cascade.detectMultiScale(gray,1.1, 5)
-        fist=fist_cascade.detectMultiScale(gray,1.3, 5)
-        okay=okay_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3,flags=0, minSize=(100,150))
+        hand=hand_cascade.detectMultiScale(gray,1.1, 5)
         thumbdown=thumbdown_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3,flags=0, minSize=(100,80))
-        #print("cascading done")
-        for (x,y,w,h) in okay:
-                cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-                roi_gray=gray[y:y+h,x:x+w]
-                roi_color=img[y:y+h,x:x+w]
-                #crop_img=img[y:y+h,x:x+w]
-                ok_crop(crop_img,img)
-                #time.sleep(0.5)
-                print("out of okay")
-                
+       
                 
         for (x,y,w,h) in point:
                 cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,255),2)
                 roi_gray=gray[y:y+h,x:x+w]
                 roi_color=img[y:y+h,x:x+w]
-                #crop_img=img[y:y+h,x:x+w]
                 ha=2
                 p_crop(crop_img,img)
-                #time.sleep(0.5)
-                print("out of point")
+                print("Slide Zoomed In!")
 
         for (x,y,w,h) in fin:
                 cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
                 roi_gray=gray[y:y+h,x:x+w]
                 roi_color=img[y:y+h,x:x+w]
-                #crop_img=img[y:y+h,x:x+w]
                 f_crop(crop_img,img)
-                #time.sleep(0.5)
-                print("out of fin")
+                time.sleep(0.5)
+                print("Slide Moved Forward!")
 
         for (x,y,w,h) in thumbdown:
                 cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
                 roi_gray=gray[y:y+h,x:x+w]
                 roi_color=img[y:y+h,x:x+w]
-                #crop_img=img[y:y+h,x:x+w]
                 ha=5
                 t_crop(crop_img,img)
-                #time.sleep(0.5)
-                print("out of thumbdown")
+                time.sleep(0.5)
+                print("Slide Moved Backwards!")
 
         for (x,y,w,h) in hand:
                 cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
                 roi_gray=gray[y:y+h,x:x+w]
                 roi_color=img[y:y+h,x:x+w]
+                h_crop(crop_img,img)
+                time.sleep(0.5)
+                print("Slide Zoomed Out!")
+
+        '''for (x,y,w,h) in fist:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                roi_gray=gray[y:y+h,x:x+w]
+                roi_color=img[y:y+h,x:x+w]
                 #crop_img=img[y:y+h,x:x+w]
                 h_crop(crop_img,img)
-                print("out of hand")
+                time.sleep(0.5)
+                print("out of FIST")'''
 
 
                 
